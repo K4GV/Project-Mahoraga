bf6b7deefb674935897a525c30cc07d6d3f912dd
K4GV <gathonikarume@gmail.com>
2024-04-08 14:57:02 +0300

    Task-3

---
+++ javascript/task-3/js/myScript.js
@@ -0,0 +1,8 @@
+// Prompt the user for their name
+var name = prompt("Please enter your name:");
+
+// Create the greeting message
+var greeting = "Greetings " + name;
+
+// Print the greeting
+console.log(greeting);
