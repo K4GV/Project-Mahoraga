1c833d0be9235b3bcc5a83cf9acd1e8d1f09b83a
K4GV <gathonikarume@gmail.com>
2024-04-22 22:32:59 +0300

    Lab three

---
+++ javascript/booleans/test/js/myScript.js
@@ -0,0 +1,9 @@
+const age = 70;
+const isHoliday = true;
+
+
+if (age <= 6 || age >= 65 && isHoliday) {
+    console.log ('Discount')
+} else{
+    console.log ('No Discount')
+}
---
+++ javascript/task-1/mySript.js
@@ -0,0 +1,5 @@
+/*
+var name = 'John';
+var age = 30;
+var isStudent = true;
+*/
